c21e914a976fa2fbd0db6dca6b2254d63f9e4a6a
Joey Baker <joey.baker@ubnt.com>
2013-07-15 13:07:45 -0700

    Add keyremapformacbook private.xml

--- link.sh
+++ link.sh
@@ -1,32 +1,40 @@
 #!/bin/sh
 # stolen from: http://www.splitbrain.org/blog/2011-02/16-managing_dotfiles_with_dropbox
- 
+
 cd `dirname $0`
 F=`pwd |sed -e "s#$HOME/\?##"`
- 
+
 for P in *
 do
     # skip files
     if [ "$P" = "link.sh" ]; then continue; fi
     if [ "$P" = "readme.md" ]; then continue; fi
- 
+    if [ "$P" = "private.xml" ]; then
+      # skip existing links
+      if [ -h "$HOME/Library/Application Support/KeyRemap4MacBook/$P" ]; then continue; fi
+      mkdir -p "$HOME/Library/Application Support/KeyRemap4MacBook/"
+      # create link
+      echo -n "Link "
+      ln -v -s "$F/$P" "$HOME/Library/Application Support/KeyRemap4MacBook/$P"
+    fi
+
     # ensure permissions
     chmod -R o-rwx,g-rwx $P
- 
+
     # skip existing links
     if [ -h "$HOME/.$P" ]; then continue; fi
- 
+
     # move existing dir out of the way
     if [ -e "$HOME/.$P" ]; then
         if [ -e "$HOME/__$P" ]; then
             echo "want to override $HOME/.$P but backup exists"
             continue;
         fi
- 
+
         echo -n "Backup "
         mv -v "$HOME/.$P" "$HOME/__$P"
     fi
- 
+
     # create link
     echo -n "Link "
     ln -v -s "$F/$P" "$HOME/.$P"
