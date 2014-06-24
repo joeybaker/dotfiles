44a9719adf56b9cba59495b2ca2e86c71ad9412e
Joey Baker <joey@byjoeybaker.com>
2014-06-24 09:33:04 -0700

    Add MAC address randomizer

---
+++ sh/macAddressRandomifiy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+macRandomizify(){
+  local INTERFACE=en0
+  local OUI="$HOME/Dropbox/dotfiles/sh/oui.txt"
+  local OUI_LEN=19004
+
+#  while [ true ]; do
+      local STATUS=`ifconfig ${INTERFACE}\
+        | grep status:\
+        | awk '{print $2}'`
+
+      if [ "$STATUS" = "inactive" ]; then
+          local R=$(((RANDOM % ${OUI_LEN})+1))
+          local PREFIX=`head -$R $OUI\
+            | tail -1`
+
+          local CMD="sudo ifconfig ${INTERFACE} ether ${PREFIX}:"
+
+          for i in `seq 1 6`; do
+              R=$(((RANDOM % 15)+1))
+              C=`echo "obase=16; $R" | bc`
+              CMD="$CMD$C"
+              if [ $((i % 2)) = 0 ] && [ $i != 6 ]; then
+                  CMD="$CMD:"
+              fi
+          done
+          local RESULT=`$CMD`
+          echo $CMD
+      else
+        echo $STATUS
+      fi
+#      sleep 30
+#  done
+}
+
+macRandomizify;
+
