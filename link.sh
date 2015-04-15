ebe5596a5dfd680cf137cff79a443ba7c016c287
Joey Baker <joey@byjoeybaker.com>
2015-04-15 11:38:12 -0700

    link script: more bash safe

--- link.sh
+++ link.sh
@@ -1,22 +1,30 @@
-#!/bin/sh
+#!/bin/bash
+# strict mode http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
 # stolen from: http://www.splitbrain.org/blog/2011-02/16-managing_dotfiles_with_dropbox
 
-cd `dirname $0`
-F=`pwd |sed -e "s#$HOME/\?##"`
+function link () {
+  cd $(dirname $0)
 
-for P in *
-do
+  local F=$(pwd | sed -e "s#$HOME/\?##")
+
+  for P in *
+  do
     # skip files
     if [ "$P" = "link.sh" ]; then continue; fi
     if [ "$P" = "readme.md" ]; then continue; fi
     if [ "$P" = "hosts" ]; then continue; fi
+    if [ "$P" = "appify" ]; then continue; fi
+    if [ "$P" = "osx" ]; then continue; fi
     if [ "$P" = "private.xml" ]; then
       # skip existing links
-      if [ -h "$HOME/Library/Application Support/KeyRemap4MacBook/$P" ]; then continue; fi
-      mkdir -p "$HOME/Library/Application Support/KeyRemap4MacBook/"
+      if [ -h "$HOME/Library/Application Support/Karabiner/$P" ]; then continue; fi
+      mkdir -p "$HOME/Library/Application Support/Karabiner/"
       # create link
       echo -n "Link "
-      ln -v -s "$F/$P" "$HOME/Library/Application Support/KeyRemap4MacBook/$P"
+      ln -v -s "$F/$P" "$HOME/Library/Application Support/Karabiner/$P"
     fi
 
     # ensure permissions
@@ -27,16 +35,18 @@ do
 
     # move existing dir out of the way
     if [ -e "$HOME/.$P" ]; then
-        if [ -e "$HOME/__$P" ]; then
-            echo "want to override $HOME/.$P but backup exists"
-            continue;
-        fi
+      if [ -e "$HOME/__$P" ]; then
+        echo "want to override $HOME/.$P but backup exists"
+        continue;
+      fi
 
-        echo -n "Backup "
-        mv -v "$HOME/.$P" "$HOME/__$P"
+      echo -n "Backup "
+      mv -v "$HOME/.$P" "$HOME/__$P"
     fi
 
     # create link
     echo -n "Link "
     ln -v -s "$F/$P" "$HOME/.$P"
-done
+  done
+}
+link $@
